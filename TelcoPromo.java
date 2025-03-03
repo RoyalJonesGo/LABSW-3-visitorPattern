9f307c2ac42473aac6f0f3044e9eb1c7b75d5955
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-03-03 09:54:45 +0800

    Update TelcoPromo.java

--- TelcoPromo.java
+++ TelcoPromo.java
@@ -1,15 +1,15 @@
 public class TelcoPromo {
     public static void main(String[] args) {
-        TelcoSubscription smart = new Telco(15, 500, Smart,false);
-        TelcoSubscription globe = new Telco(10, 450, Globe,true);
-        TelcoSubscription ditto = new Telco(8, 400, Ditto,true);
+        TelcoSubscription smart = new Telco(15, 500, "Smart",false);
+        TelcoSubscription globe = new Telco(10, 450, "Globe",true);
+        TelcoSubscription ditto = new Telco(8, 400, "Ditto",true);
 
         UsagePromo promo = new TelcoAllowance();
-        UnliCallOffer unli = new UnliCallTextPackage();
+        UnliCallsTextOffer unli = new UnliCallTextPackage();
 
         System.out.println("Smart Data Usage Offer and price: " + promo.showAllowance(smart.getTelcoName(), smart.getPromoPrice()));
-        System.out.println("Globe Data Usage Offer and price" + promo.showAllowance(globe.getTelcoName(), globe.getPromoPrice()));
-        System.out.println("Ditto Data Usage Offer and price" + promo.showAllowance(ditto.getTelcoName(), ditto.getPromoPrice()));
+        System.out.println("Globe Data Usage Offer and price: " + promo.showAllowance(globe.getTelcoName(), globe.getPromoPrice()));
+        System.out.println("Ditto Data Usage Offer and price: " + promo.showAllowance(ditto.getTelcoName(), ditto.getPromoPrice()));
 
         System.out.println("\nSmart unlimited calls and text package: " +
 
